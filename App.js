2328137c268814bf43f6d43204cfca0579d7a9b3
shokkun47 <shoki1310@gmail.com>
2024-08-28 23:55:56 +0900

    add new fiels

--- App.js
+++ App.js
@@ -1,22 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, FlatList, } from 'react-native';
 import { ListItem } from "./components/ListItem";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+const URL = "https://newsapi.org/v2/top-headlines?country=jp&apiKey=0c433aa615d94b718b08df0a47b706f2";
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchArticles()
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <ListItem 
-        imageURL={"https://picsum.photos/id/10/300/300"}
-        title="aaaaaaaaaaaaaaaaaaaaaaaaa"
-        author="1つめのニュース"
-      />
-      <ListItem 
-        imageURL={"https://picsum.photos/id/20/300/300"}
-        title="iiiiiiiiiiiiiiiii"
-        author="2つ目のニュース"
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem imageURL={item.urlToImage}  title={item.title} author={item.author} />
+        )}
+        keyExtractor={(item, index) => index.toString()}
       />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -24,7 +41,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#eee',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 })
