7d3a25ef385e99152e78ab26c74236249c83a6c0
shokkun47 <shoki1310@gmail.com>
2024-08-30 13:30:47 +0900

    add reducer files

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { ArticleScreen } from './screens/ArticleScreen';
 import { ClipScreen } from './screens/ClipScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { FontAwesome } from "@expo/vector-icons";
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -30,11 +32,13 @@ const HomeStack = () => {
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={screenOption}>
-        <Tab.Screen name="HomeTab" component={HomeStack} options={{ headerShown: false, title: "Home" }} />
-        <Tab.Screen name="ClipTab" component={ClipScreen} options={{ headerShown: false, title: "Clip" }} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={screenOption}>
+          <Tab.Screen name="HomeTab" component={HomeStack} options={{ headerShown: false, title: "Home" }} />
+          <Tab.Screen name="ClipTab" component={ClipScreen} options={{ headerShown: false, title: "Clip" }} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
---
+++ store/userSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    clips: []
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    addClip: (state, action) => {
+        const newClip = action.payload;
+        state.clips.push(newClip)
+    },
+    deleteClip: (state, action) => {
+        const deleteClip = action.payload;
+        const currentClips = state.clips;
+        const filteredClips = currentClips.filter((clip) => clip.url != deleteClip.url);
+        state.clips = filteredClips;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addClip, deleteClip } = counterSlice.actions;
+
+export default userSlice.reducer;
